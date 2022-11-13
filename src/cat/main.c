35850aa0cffdfe1409108f8c19c63b659be68695
edeotena <edeotena13@gmail.com>
2022-11-13 17:14:35 +0300

    Some logical and style improvements

--- src/cat/main.c
+++ src/cat/main.c
@@ -9,8 +9,26 @@ typedef struct {
     int vOpt;
 } CatOptions;
 
-void catNoOptions(char *filename) {
-    FILE* file = fopen(filename, "r");
+void catNoOptions(const char *filename);
+
+CatOptions getOptions(int argc, char *argv[], int *filename_index);
+
+void catWithOptions(const char *filename, const CatOptions *options);
+
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        printf("n/a");
+    } else if (argc == 2) {
+        catNoOptions(argv[1]);
+    } else {
+        int filename_index = 0;
+        CatOptions options = getOptions(argc, argv, &filename_index);
+        catWithOptions(argv[filename_index], &options);
+    }
+}
+
+void catNoOptions(const char *filename) {
+    FILE *file = fopen(filename, "r");
     if (file == NULL) {
         printf("n/a");
     } else {
@@ -18,21 +36,5 @@ void catNoOptions(char *filename) {
         while ((c = getc(file)) != EOF) {
             printf("%c", c);
         }
-    }
-}
-
-void catWithOptions(char *argv[], CatOptions *options) {
-
-}
-
-int main(int argc, char* argv[]) {
-    if (argc == 1) {
-        printf("n/a");
-    }
-    else if (argc == 2) {
-        catNoOptions(argv[1]);
-    } else {
-        CatOptions options  ;
-        catWithOptions(argv, &options);
     }
 }
