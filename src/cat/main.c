2d9b5a10e7e969d5551afcad8e3702ffee185229
edeotena <edeotena13@gmail.com>
2022-11-13 19:00:15 +0300

    Options getting realized

--- src/cat/main.c
+++ src/cat/main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <string.h>
+
+#define BAD_OPTIONS (-1)
 
 typedef struct {
     int eOpt;
@@ -11,9 +14,9 @@ typedef struct {
 
 void catNoOptions(const char *filename);
 
-CatOptions getOptions(int argc, char *argv[], int *filename_index);
+CatOptions getOptions(int argc, char *argv[], int *code);
 
-void catWithOptions(const char *filename, const CatOptions *options);
+void catWithOptions(char *argv[], const CatOptions *options);
 
 int main(int argc, char *argv[]) {
     if (argc == 1) {
@@ -21,9 +24,13 @@ int main(int argc, char *argv[]) {
     } else if (argc == 2) {
         catNoOptions(argv[1]);
     } else {
-        int filename_index = 0;
-        CatOptions options = getOptions(argc, argv, &filename_index);
-        catWithOptions(argv[filename_index], &options);
+        int filename_index = 0, code = 0;
+        CatOptions options = getOptions(argc, argv, &code);
+        if (code != BAD_OPTIONS) {
+            catWithOptions(argv, &options);
+        } else {
+            printf("n/a");
+        }
     }
 }
 
@@ -33,6 +40,60 @@ void catNoOptions(const char *filename) {
         printf("n/a");
     } else {
         int c = EOF;
+        while ((c = getc(file)) != EOF) {
+            printf("%c", c);
+        }
+    }
+}
+
+CatOptions getOptions(int argc, char *argv[], int *code) {
+    CatOptions result;
+    for (int i = 1; i < argc && *code != BAD_OPTIONS; ++i) {
+        if (argv[i][0] == '-' && argv[i][1] != '-') {
+            size_t len = strlen(argv[i]);
+            for (size_t j = 1; j < len && *code != BAD_OPTIONS; ++j) {
+                if (argv[i][j] == 'b') {
+                    result.bOpt = 1;
+                } else if (argv[i][j] == 'e') {
+                    result.eOpt = 1;
+                    result.vOpt = 1;
+                } else if (argv[i][j] == 'E') {
+                    result.eOpt = 1;
+                } else if (argv[i][j] == 'n') {
+                    result.nOpt = 1;
+                } else if (argv[i][j] == 's') {
+                    result.sOpt = 1;
+                } else if (argv[i][j] == 't') {
+                    result.tOpt = 1;
+                    result.vOpt = 1;
+                } else if (argv[i][j] == 'T') {
+                    result.tOpt = 1;
+                } else {
+                    *code = BAD_OPTIONS;
+                }
+            }
+            argv[i][0] = '\0';
+        } else if (argv[i][1] == '-') {
+            if (strcmp(argv[i], "--number-nonblank") == 0) {
+                result.bOpt = 1;
+            } else if (strcmp(argv[i], "--number") == 0) {
+                result.nOpt = 1;
+            } else if (strcmp(argv[i], "--squeeze-blank") == 0) {
+                result.sOpt = 1;
+            } else {
+                *code = BAD_OPTIONS;
+            }
+            argv[i][0] = '\0';
+        }
+    }
+}
+
+void catWithOptions(char *argv[], const CatOptions *options) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("n/a");
+    } else {
+        int c = EOF, last_char ;
         while ((c = getc(file)) != EOF) {
             printf("%c", c);
         }
