7e8578794c420d7c598de556204fe03c2c1f0a26
Guini Cybernetic <guinicyb@mi-r6.msk.21-school.ru>
2022-11-18 17:34:28 +0300

    Fixed cat errors && added tests

--- src/cat/main.c
+++ src/cat/main.c
@@ -36,7 +36,8 @@ int main(int argc, char *argv[]) {
 
 CatOptions getOptions(int argc, char *argv[], int *code) {
     CatOptions result = {};
-    for (int i = 1; i < argc && *code == 0; ++i) {
+    int file_found = 0, i;
+    for (i = 1; i < argc && *code == 0 && file_found == 0; ++i) {
         if (argv[i][0] == '-' && argv[i][1] != '-') {
             size_t len = strlen(argv[i]);
             for (size_t j = 1; j < len && *code == 0; ++j) {
@@ -56,12 +57,14 @@ CatOptions getOptions(int argc, char *argv[], int *code) {
                     result.vOpt = 1;
                 } else if (argv[i][j] == 'T') {
                     result.tOpt = 1;
+                } else if (argv[i][j] == 'v') {
+                    result.vOpt = 1;
                 } else {
                     *code = BAD_OPTIONS;
                 }
             }
             argv[i][0] = '\0';
-        } else if (argv[i][1] == '-') {
+        } else if (argv[i][0] == '-' && argv[i][1] == '-') {
             if (strcmp(argv[i], "--number-nonblank") == 0) {
                 result.bOpt = 1;
             } else if (strcmp(argv[i], "--number") == 0) {
@@ -72,6 +75,8 @@ CatOptions getOptions(int argc, char *argv[], int *code) {
                 *code = BAD_OPTIONS;
             }
             argv[i][0] = '\0';
+        } else {
+            file_found = 1;
         }
     }
     return result;
@@ -83,8 +88,9 @@ void catWithOptions(int argc, char *argv[], const CatOptions *options) {
         if (argv[i][0] != '\0') {
             FILE *file = fopen(argv[i], "r");
             if (file != NULL) {
-                char ch, last_ch = '\n';
-                while ((ch = (char) fgetc(file)) != EOF) {
+                string_number = 1;
+                int ch, last_ch = '\n';
+                while ((ch = getc(file)) != EOF) {
                     if (options->sOpt != 1 || ch != '\n' || empty_string != 1) {
                         if ((options->bOpt == 1 && last_ch == '\n' && ch != '\n') ||
                             (options->nOpt == 1 && options->bOpt != 1 && last_ch == '\n')) {
@@ -97,14 +103,16 @@ void catWithOptions(int argc, char *argv[], const CatOptions *options) {
                         }
                         if (options->tOpt == 1 && ch == '\t') {
                             printf("^I");
-                        } else if (options->vOpt == 1 && !isascii(ch)) {
-                            printf("M-");
-                            ch = toascii(ch);
+                        } else if (options->vOpt == 1 && ch != '\n' && ch != '\t') {
+                            if (!isascii(ch)) {
+                                printf("M-");
+                                ch = toascii(ch);
+                            }
                             if (iscntrl(ch)) {
                                 putchar('^');
                                 putchar(ch == '\177' ? '?' : ch | 0100);
                             } else {
-                                printf("%c", ch);
+                                putchar(ch);
                             }
                         } else {
                             putchar(ch);
